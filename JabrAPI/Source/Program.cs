354b958ba32bd9a526fd78da22b76a58ed011c88
Gyro <gyroscopic.why@gmail.com>
2025-08-01 19:30:22 +0300

    Fix demo parameters

--- JabrAPI/Source/Program.cs
+++ JabrAPI/Source/Program.cs
@@ -14,7 +14,7 @@ namespace JabrAPI
             //string aboba = "aboba";
             //RE5.EncryptionKey reKey = new RE5.EncryptionKey();
 
-            RE5.EncryptionKey reKey = new RE5.EncryptionKey("abcqwertyuiop1234567", "aboba123", 3);
+            RE5.EncryptionKey reKey = new RE5.EncryptionKey("abcqwertyuiop1234567", "abolk123", 3);
             string msg = "aboba";
             Write("\n\tMessage:   (" + msg.Length     + ") " + msg);
             Write("\n\tPrimary:   (" + reKey.PrLength + ") " + reKey.PrimaryAlphabet);
@@ -25,7 +25,7 @@ namespace JabrAPI
             for (Int32 curShift = 0; curShift < reKey.Shifts.Length; curShift++) 
                 Write(reKey.Shifts[curShift] + " ");*/
 
-            string enc  = RE5.Encrypt(msg, reKey);
+            string enc  = RE5.FastEncrypt(msg, reKey);
             //string test = RE5.EncryptWithConsoleInfo(msg, reKey);
             
             //Demo.AlgorithmTest(enc, test);
