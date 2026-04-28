98ae01c24b594cf4767a1f6607e3557b4dec4a5e
Gyro <gyroscopic.why@gmail.com>
2026-04-28 10:40:40 +0300

    [TESTED] +Noise.Remove.BinaryFile

--- JabrAPI/Source/Noise/Remove/Binary/External.cs
+++ JabrAPI/Source/Noise/Remove/Binary/External.cs
@@ -35,7 +35,6 @@ namespace JabrAPI
                 return result;
             }
 
-
             static public List<Byte> Binary(List<Byte> noised, BinaryNoisifier noisifier,
                 out Exception? exception)
             {
@@ -58,16 +57,85 @@ namespace JabrAPI
             }
 
 
+            static public bool BinaryFile(string absoluteInputDirectory, string fileName,
+                string absoluteOutputDirectory, IBinaryKey reKey,
+                    out Exception? exception)
+            {
+                if (IsReKeyValid(reKey, out exception) &&
+                    IsNoisifierValid(reKey.Noisifier, out exception))
+                {
+                    //try
+                    //{
+                        FastBinaryFile(absoluteInputDirectory, fileName, absoluteOutputDirectory, reKey.Noisifier);
+                        return true;
+                    //}
+                    //catch (Exception innerException) { exception = innerException; }
+                }
+                return false;
+            }
+            static public bool BinaryFile(string absoluteInputDirectory, string fileName,
+                string absoluteOutputDirectory, IBinaryKey reKey,
+                bool throwExceptions = false)
+            {
+                bool result = BinaryFile(absoluteInputDirectory, fileName, absoluteOutputDirectory, reKey, out Exception? exception);
+                if (!result && throwExceptions) throw exception!;
+                return result;
+            }
+            static public bool BinaryFile(string absoluteInputDirectory, string fileName,
+                string absoluteOutputDirectory, BinaryNoisifier noisifier,
+                    out Exception? exception)
+            {
+                if (IsNoisifierValid(noisifier, out exception))
+                {
+                    try
+                    {
+                        FastBinaryFile(absoluteInputDirectory, fileName, absoluteOutputDirectory, noisifier);
+                        return true;
+                    }
+                    catch (Exception innerException) { exception = innerException; }
+                }
+                return false;
+            }
+            static public bool BinaryFile(string absoluteInputDirectory, string fileName,
+                string absoluteOutputDirectory, BinaryNoisifier noisifier,
+                bool throwExceptions = false)
+            {
+                bool result = BinaryFile(absoluteInputDirectory, fileName, absoluteOutputDirectory, noisifier, out Exception? exception);
+                if (!result && throwExceptions) throw exception!;
+                return result;
+            }
+
+            static public bool BinaryFile(string absoluteInputDirectory, string fileName, IBinaryKey reKey, out Exception? exception)
+                => BinaryFile(absoluteInputDirectory, fileName, absoluteInputDirectory, reKey, out exception);
+            static public bool BinaryFile(string absoluteInputDirectory, string fileName, IBinaryKey reKey, bool throwExceptions = false)
+                => BinaryFile(absoluteInputDirectory, fileName, absoluteInputDirectory, reKey, throwExceptions);
+            static public bool BinaryFile(string absoluteInputDirectory, string fileName, BinaryNoisifier noisifier, out Exception? exception)
+                => BinaryFile(absoluteInputDirectory, fileName, absoluteInputDirectory, noisifier, out exception);
+            static public bool BinaryFile(string absoluteInputDirectory, string fileName, BinaryNoisifier noisifier, bool throwExceptions = false)
+                => BinaryFile(absoluteInputDirectory, fileName, absoluteInputDirectory, noisifier, throwExceptions);
+
+
 
             static public List<Byte> FastBinary(List<Byte> noised, BinaryNoisifier noisifier)
             {
                 return
                 [.. Internal.RemoveFastBinary
-                (
-                    noised,
-                    noisifier
-                )
+                    (
+                        noised,
+                        noisifier
+                    )
                 ];
+            }
+            static public void FastBinaryFile(string absoluteInputDirectory, string fileName,
+                string absoluteOutputDirectory, BinaryNoisifier noisifier)
+            {
+                Internal.RemoveFastBinaryFile
+                (
+                    absoluteInputDirectory,
+                    fileName,
+                    absoluteOutputDirectory,
+                    noisifier
+                );
             }
         }
     }
--- JabrAPI/Source/Noise/Remove/Binary/InternalMain.cs
+++ JabrAPI/Source/Noise/Remove/Binary/InternalMain.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 
 
@@ -58,6 +59,53 @@ namespace JabrAPI
                 }
 
                 return result;
+            }
+
+
+
+            static public void RemoveFastBinaryFile(string absoluteInputDirectory,
+                string fileName, string absoluteOutputDirectory, BinaryNoisifier noisifier)
+            {
+                List<Byte> primary = noisifier.PrimaryNoise, complex = noisifier.ComplexNoise;
+                string finalFileName;
+
+                Int32 chunkSize = (Int32)noisifier.settings.ChunkSize;
+                if   (chunkSize < 2) chunkSize = 2;
+
+
+                if (!noisifier.settings.KeepOriginalFileExtension)
+                {
+                    finalFileName = Path.ChangeExtension(fileName, "dnoisev5");
+                    for (var i = 1; File.Exists(Path.Combine(absoluteOutputDirectory, finalFileName)); i++)
+                        finalFileName = Path.ChangeExtension(fileName, $"dnoisev5-{i}");
+                }
+                else finalFileName = fileName + ".dnoisev5";
+
+
+                using FileStream inputStream  = new(Path.Combine(absoluteInputDirectory,  fileName),      FileMode.Open,   FileAccess.Read);
+                using FileStream outputStream = new(Path.Combine(absoluteOutputDirectory, finalFileName), FileMode.Create, FileAccess.Write);
+
+                using BinaryReader reader = new(inputStream);
+                using BinaryWriter writer = new(outputStream);
+
+
+                Int32 bytesRead;
+                bool ignoringIsActive = false;
+                Byte[] noisedChunk = new Byte[chunkSize];
+
+                while ((bytesRead = reader.Read(noisedChunk, 0, chunkSize)) > 0)
+                {
+                    writer.Write
+                    (
+                        RemovalRound
+                        (
+                            [.. noisedChunk[0..bytesRead]],
+                            ref ignoringIsActive,
+                            primary,
+                            complex
+                        )
+                    );
+                }
             }
         }
     }
